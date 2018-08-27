e2a1c9be44d1f10c46486198fdf3b6e35987fe5e
Hideki Sekine <sekineh@me.com>
2018-08-27 20:42:01 +0900

    [ci] linux: add --features full

--- ci/script.sh
+++ ci/script.sh
@@ -5,14 +5,18 @@ set -ex
 # TODO This is the "test phase", tweak it as you see fit
 main() {
     cargo build --target $TARGET
+    cargo build --target $TARGET --features full
     cargo build --target $TARGET --release
+    cargo build --target $TARGET --release --features full
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
     cargo test --target $TARGET
+    cargo test --target $TARGET --features full
     cargo test --target $TARGET --release
+    cargo test --target $TARGET --release --features full
 }
 
 # we don't run the "test phase" when doing deploys
